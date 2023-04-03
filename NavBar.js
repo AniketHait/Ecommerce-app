a513d63635819c179c4f7baf4a73a210bb7c2fbb
AniketHait <42712707+AniketHait@users.noreply.github.com>
2023-04-03 10:02:10 +0530

    Add files via upload

---
+++ NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './styles.css'
+
+
+<link href="styles.css" rel="stylesheet"></link>
+const Navbar =()=>{
+    return(
+        <div className="container">
+            <div className="wrapper">
+              EN
+                <div className="Center">
+                  <strong>Shopping App</strong>
+                 </div>
+                 <div>
+                    REGISTER/SIGN IN
+                 </div>
+                 <div>Add Products</div>
+                </div>
+            
+        </div>
+    )
+}
+
+export default Navbar;
